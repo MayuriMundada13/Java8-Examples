0a4257e37c3d536daf3daa3f34734fd7c7f17c10
mayuri_mundada <mayurimundada13@gmail.com>
2024-05-08 19:00:01 +0530

    Committing 8/5/2024 6:59 PM

---
+++ src/main/java/org/interview/Example1.java
@@ -0,0 +1,26 @@
+package org.interview;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+//asking this example in Yash internal evaluation interview
+
+public class Example1 {
+    public static List<Integer> getNumber() {
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(1);
+        integerList.add(16);
+        integerList.add(5);
+        integerList.add(7);
+        integerList.add(6);
+
+        return integerList.stream()
+                 .filter(number -> String.valueOf(number).startsWith("1"))
+                 .collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Number = " + getNumber());
+    }
+}
---
+++ src/main/java/org/practices/Example1.java
@@ -0,0 +1,34 @@
+package org.practices;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//convert array list in stream using collect & forEach
+
+public class Example1 {
+
+    public static List<String> convertArrayListInStream() {
+        List<String> stringList = new ArrayList<>();
+        stringList.add("Mayuri");
+        stringList.add("Gauri");
+        stringList.add("Radhika");
+        stringList.add("Pooja");
+        stringList.add("Mundada");
+
+        //display string that start with m
+        /*
+        return stringList.stream()
+                .filter(m -> m.toLowerCase().startsWith("m"))
+                .collect(Collectors.toList());
+         */
+        List<String> list = new ArrayList<>();
+        stringList.stream()
+                .filter(m -> m.toLowerCase().startsWith("m"))
+                .forEach(list::add);
+        return list;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(convertArrayListInStream());
+    }
+}
