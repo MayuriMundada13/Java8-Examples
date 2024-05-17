5fee953905ed6ba38a101d2ebcd75dc585cb57d3
mayuri_mundada <mayurimundada13@gmail.com>
2024-05-17 16:43:44 +0530

    Committing JD TCI interview question - 17/05/2024 4:43 PM

---
+++ src/main/java/org/interview/JdInterview.java
@@ -0,0 +1,18 @@
+package org.interview;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class JdInterview {
+    public static void main(String[] args) {
+        String input = "en_lawn_tractors";
+        String[] s = input.split("_");
+
+        String collect = Arrays.stream(s)
+                .skip(1)
+                .map(name -> name.substring(0, 1).toUpperCase() + name.substring(1))
+                .collect(Collectors.joining(" "));
+
+        System.out.println("Output = " + collect);
+    }
+}
